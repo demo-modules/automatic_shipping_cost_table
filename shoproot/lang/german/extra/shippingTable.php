c86597961038513be6883068903040f3425c5fc5
demo-modules <120286459+demo-modules@users.noreply.github.com>
2023-04-02 13:50:02 +0200

    Create shippingTable.php

---
+++ shoproot/lang/german/extra/shippingTable.php
@@ -0,0 +1,17 @@
+<?php
+defined('TOP') or define('TOP', 'zur&uuml;ck zum Seitenanfang');
+defined('CHOOSE_YOUR_OPTION') or define('CHOOSE_YOUR_OPTION', 'W&auml;hlen Sie hier Ihren bevorzugten Versanddienstleister aus:');
+defined('SHIPPING_ZONES') or define('SHIPPING_ZONES', 'Versandzonen:');
+defined('COUNTRY_SHIPPING') or define('COUNTRY_SHIPPING', 'Versand nach:');
+defined('COUNTRY_COST') or define('COUNTRY_COST', 'Kosten:');
+defined('COUNTRY_COST_EUR') or define('COUNTRY_COST_EUR', ' EUR');
+defined('COUNTRY_COST_BY') or define('COUNTRY_COST_BY', 'ab ');
+defined('COUNTRY_WEIGHT') or define('COUNTRY_WEIGHT', ' kg');
+defined('COUNTRY_COST_FLAT') or define('COUNTRY_COST_FLAT', 'pauschal');
+defined('ECO') or define('ECO', 'Economy');
+defined('PRI') or define('PRI', 'Priority');
+defined('URG') or define('URG', 'Urgent');
+defined('COUNTRY_SHIPPING_FALSE') or define('COUNTRY_SHIPPING_FALSE', 'Momentan k&ouml;nnen die Versandkosten nicht berechnet werden!');
+defined('COUNTRY_SHIPPING_HANDLING_TEXT') or define('COUNTRY_SHIPPING_HANDLING_TEXT', 'Die Bearbeitungsgeb&uuml;hr f&uuml;r diese Versandart betr&auml;gt: ');
+defined('COUNTRY_SHIPPING_ALLOWED_TEXT') or define('COUNTRY_SHIPPING_ALLOWED_TEXT', 'Wir versenden mit dieser Versandart nur in folgende L&auml;nder:');
+?>
